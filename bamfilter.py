6a5806141ac121e89da93d85a0e6ab850bfd42d7
Eitan Halper-Stromberg <Eitan177@gmail.com>
2024-02-29 17:06:24 -0500

    Update bamfilter.py

--- bamfilter.py
+++ bamfilter.py
@@ -51,9 +51,10 @@ if submitted:
                 reads_use = reads_use[mask] 
                 st.write(mask)
             else:
-                mask = reads_use.map(lambda x: query in str(x).lower()).any(axis=1) 
+                mask = reads_use.map(lambda x: not query in str(x).lower())
                 st.write('here')
-                reads_use= reads_use[~reads_use.index.isin(mask)]
+                st.write(mask)
+                reads_use = reads_use[mask]
                 #reads_use = pd.merge(reads_use,reads_use[mask], how='left') 
         st.data_editor(reads_use)
         st.write(reads_use.shape)
